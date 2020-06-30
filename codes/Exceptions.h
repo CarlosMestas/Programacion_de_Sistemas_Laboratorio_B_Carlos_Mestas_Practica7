c67a7c6b882d572f963e6f071fa0361099214298
CarlosMestas <42045414+CarlosMestas@users.noreply.github.com>
2020-06-30 17:59:07 -0500

    Final commit

---
+++ codes/Exceptions.h
@@ -0,0 +1,44 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <cstring>	
+#include <iostream>
+// Clase para la exepcion
+class	MyException	{
+public:
+	// Almacenara el mensaje de error
+	char msg[40];
+	MyException(){
+		*msg = 0;	
+	}
+	MyException(const char *msg2) {
+		strcpy(this->msg, msg2);
+	}
+};
+
+class ComputerException {
+public:
+	char mesage[50];
+};
+
+class InputException: public ComputerException{
+public:
+	InputException(const char *msg2) {
+		strcpy(this->mesage, msg2);
+	}
+};
+
+class ProcessorException: public ComputerException{
+public:
+	ProcessorException(const char *msg2) {
+		strcpy(this->mesage, msg2);
+	}
+};
+
+class OutputException: public ComputerException{
+public:
+	OutputException(const char *msg2) {
+		strcpy(this->mesage, msg2);
+	}
+};
+#endif
---
+++ codes/Exercise_5.cpp
@@ -0,0 +1,41 @@
+// Exercise_5.cpp : Desarrollo del ejercicio 5
+// Curso: Programacion de Sistemas
+// Laboratorio: B
+// Autor: Carlos Alberto Mestas Escarcena
+// Fecha: 30/06/2020
+
+#include <iostream>
+#include <fstream>
+#include "Exceptions.h"
+
+using namespace std;
+int main(){
+	system("color f0");
+	bool y = false;
+	do{
+		try	{
+			string fileNameText;
+			cout << "\nIngrese el nombre del fichero para imprimirlo: \n";
+			cin >> fileNameText;
+			std::ifstream file(fileNameText);
+			if (file.is_open()) {
+				y = true;
+				std::string line;
+				while (std::getline(file, line)) {
+					printf("%s", line.c_str());
+				}
+				file.close();
+			}
+			else{
+				throw InputException("No se encontro el fichero ...");
+			}
+		}
+		catch(InputException e){
+			cerr <<	"Error:	" << e.mesage;	
+		}
+		
+	}
+	while(y == false);
+	
+	return 0;
+}
